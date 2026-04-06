285adbe0453d59829704a8ec0511ca3285819e24
Fellipe Bordin <fellipebordin@hotmail.com>
2026-04-06 01:02:27 -0300

    fix: migrate prisma client import for prisma 7

--- Next-API/src/lib/prisma.ts
+++ Next-API/src/lib/prisma.ts
@@ -1,22 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient;
 };
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg(pool);
+if (!connectionString) {
+  throw new Error("DATABASE_URL não definida.");
+}
+
+const adapter = new PrismaPg({
+  connectionString,
+});
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-    log: ["warn", "error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
